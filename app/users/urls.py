eef74887d2893f75f817fd2f759733224e1a04d0
prxy <prxykanye@prxyya.ru>
2026-04-15 22:26:16 +0700

    init

---
+++ app/users/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import LoginView, LogoutView, HomeView
+
+urlpatterns = [
+    path("", HomeView.as_view(), name="home"),
+    path("login/", LoginView.as_view(), name="login"),
+    path("logout/", LogoutView.as_view(), name="logout"),
+]
